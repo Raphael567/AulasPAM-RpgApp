1cca70d2603d40b437b86b72ea163cb6d91048e3
Raphael567 <raphaelcanuto464@gmail.com>
2025-06-02 08:56:58 -0300

    Aula 15

--- AppRpgEtec/AppRpgEtec/Services/Personagens/PersonagemService.cs
+++ AppRpgEtec/AppRpgEtec/Services/Personagens/PersonagemService.cs
@@ -11,7 +11,7 @@ namespace AppRpgEtec.Services.Personagens
     public class PersonagemService : Request
     {
         private readonly Request _request;
-        private const string apiUrlBase = "hhtps://rpgraphael-dvejarfmgyfycwba.brazilsouth-01.azurewebsites.net/Personagens";
+        private const string apiUrlBase = "https://rpgraphael-dvejarfmgyfycwba.brazilsouth-01.azurewebsites.net/Personagens";
 
         private string _token;
         public PersonagemService(string token)
